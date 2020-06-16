99471c80489d861f2d2768b76fa4ae644e158868
Kartik <40801023+kartik4all@users.noreply.github.com>
2020-06-16 17:07:16 +0530

    responsive web resume

---
+++ precache-manifest.31342895d3d8902409bb23e6aeb8def8.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "416891b9e71c49546fb6c6e52d6162d9",
+    "url": "/web_resume/index.html"
+  },
+  {
+    "revision": "93beffaaa82d8a8c92fb",
+    "url": "/web_resume/static/css/main.ee6d1070.chunk.css"
+  },
+  {
+    "revision": "a1a4438b5dfd7a4164a8",
+    "url": "/web_resume/static/js/2.8524ccc4.chunk.js"
+  },
+  {
+    "revision": "3aba3a90632230c98c06faa53d3a257f",
+    "url": "/web_resume/static/js/2.8524ccc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93beffaaa82d8a8c92fb",
+    "url": "/web_resume/static/js/main.49a14db0.chunk.js"
+  },
+  {
+    "revision": "a611d05b280723f1751b",
+    "url": "/web_resume/static/js/runtime-main.eb5ea97d.js"
+  },
+  {
+    "revision": "dc1833f129f38bfa820448e0a411844b",
+    "url": "/web_resume/static/media/IMG-20190503-WA0058.dc1833f1.jpg"
+  },
+  {
+    "revision": "edebc3561ed2b0d82c9682dcb8b8b79b",
+    "url": "/web_resume/static/media/appleclone.edebc356.jfif"
+  },
+  {
+    "revision": "bf28cefbaf2370100c941f11cf715cfb",
+    "url": "/web_resume/static/media/bcci_clone.bf28cefb.jfif"
+  },
+  {
+    "revision": "0c6fb74686569998e1d65468dfb41d06",
+    "url": "/web_resume/static/media/bookmyshow_clone.0c6fb746.jfif"
+  },
+  {
+    "revision": "0d0dffc8b920274fb20e8bfa4fe63fc3",
+    "url": "/web_resume/static/media/claro-agroAnalysis.0d0dffc8.jfif"
+  },
+  {
+    "revision": "95234b15c86ec0daa3d9e2b1df0f50c4",
+    "url": "/web_resume/static/media/claro-agroform.95234b15.jfif"
+  },
+  {
+    "revision": "a750a569957c4a6a7257e098a3fcd7b9",
+    "url": "/web_resume/static/media/claro-farmer.a750a569.jfif"
+  },
+  {
+    "revision": "ce4f60b14717599772a13d72d83302d8",
+    "url": "/web_resume/static/media/claro-hc.ce4f60b1.jfif"
+  },
+  {
+    "revision": "a704be982ca688924c79f9aa20e12d4e",
+    "url": "/web_resume/static/media/claro-home.a704be98.jfif"
+  },
+  {
+    "revision": "2b76d4be33ca3aa0c2a808079e95a8ff",
+    "url": "/web_resume/static/media/claro-iaas.2b76d4be.jfif"
+  },
+  {
+    "revision": "2fb7aa5ddf874a51cb101b903349f8fc",
+    "url": "/web_resume/static/media/claro-mandiDashboard.2fb7aa5d.jfif"
+  },
+  {
+    "revision": "3e0efe27763ae7d301b3f8f0a97f66e4",
+    "url": "/web_resume/static/media/claro-rms.3e0efe27.jfif"
+  },
+  {
+    "revision": "fcc22b8ff6d8f934a8eeafe87187fe4d",
+    "url": "/web_resume/static/media/entry.fcc22b8f.jfif"
+  },
+  {
+    "revision": "5d73efa1df8570de5eaad12253944168",
+    "url": "/web_resume/static/media/gaming.5d73efa1.jfif"
+  },
+  {
+    "revision": "e1011214bb2403423610800321464180",
+    "url": "/web_resume/static/media/iitbact.e1011214.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/web_resume/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "5e4fe600f727574c9786cfe9a0912358",
+    "url": "/web_resume/static/media/ogt.5e4fe600.jfif"
+  },
+  {
+    "revision": "57e8b8696307205c788aa44fef431172",
+    "url": "/web_resume/static/media/sabjiwalababa.57e8b869.jpg"
+  },
+  {
+    "revision": "18eeee06f8df52a638a7c15f8601be8a",
+    "url": "/web_resume/static/media/sabjiwalababaAdmin.18eeee06.jpg"
+  },
+  {
+    "revision": "4871068fc80b7e7e953ef4215341c3a6",
+    "url": "/web_resume/static/media/vendor.4871068f.jfif"
+  },
+  {
+    "revision": "6641804f86c9ce20909abc5a2ac80af0",
+    "url": "/web_resume/static/media/wynkClone.6641804f.jfif"
+  }
+]);
